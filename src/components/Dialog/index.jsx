31c4215703f53bb09795123ff99521b031a45c81
Diego Silveira <diego.silveira@ngi.com.br>
2026-02-04 21:05:30 -0300

    feat: new component Text Input and our styles

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -29,7 +29,7 @@ export function Dialog({ isOpen, onClose, children }) {
             <IconClose />
           </button>
         </div>
-        {children}
+        <div className="body">{children}</div>
       </dialog>
     </>
   );
